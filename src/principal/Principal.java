aad0426641bd9eed2798ad64e2b65a9950fce745
mbmoncalvillo <mb.moncalvillo@irakasle.eus>
2025-10-27 13:33:28 +0100

    primmer commit

---
+++ src/principal/Principal.java
@@ -0,0 +1,12 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Principal {
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		Scanner teclado=new Scanner(System.in);
+		teclado.nextLine();
+		teclado.close();
+	}
+}
